2f60fcce4d10d93ea5cc10685a198cc6719c264e
Akeem Amusat <aoamusat@gmail.com>
2023-10-12 03:55:43 +0100

    Add user signup using AppWrite Account SDK

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const { Client } = Appwrite;
+const { Client, Account, ID } = Appwrite;
 
 const client = new Client();
 
 client.setEndpoint("https://cloud.appwrite.io/v1");
 client.setProject("652652cd9c86481a80c9");
 
-console.log({ APP_WRITE_CLIENT: client });
+const account = new Account(client);
+
+const enroll = (event) => {
+  try {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+
+    // Register User
+    account.create(ID.unique(), email, phone, name).then(
+      (response) => {
+        alert("Registration successful");
+        console.log(response);
+      },
+      (err) => {
+        alert(err.message);
+        console.log(err);
+      },
+    );
+
+    const promise = account.createVerification("http://localhost");
+    promise.then(
+      function (response) {
+        console.log(response); // Success
+      },
+      function (error) {
+        console.log(error); // Failure
+      },
+    );
+  } catch (error) {
+    alert(error.message);
+    console.log(error);
+  }
+};
